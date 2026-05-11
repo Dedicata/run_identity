5546155be1c50d27e5c34397e3bc1f68e3e2d43e
Renan Teixeira <git@renanteixeira.com.br>
2026-05-11 18:42:32 -0300

    chore(18.0): set version 18.0.1.0.0 and update website

--- dedicata_run_identity/__manifest__.py
+++ dedicata_run_identity/__manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Dedicata Run Identity Provisioning",
     "summary": "Provision Odoo users managed by Dedicata Run and Keycloak OIDC",
-    "version": "@@ODOO_MODULE_VERSION@@",
+    "version": "18.0.1.0.0",
     "category": "Tools",
     "license": "AGPL-3",
     "author": "Dedicata",
-    "website": "https://github.com/dedicata/odoo",
+    "website": "https://github.com/dedicata/run_identity",
     "depends": ["dedicata_auth_oidc_keycloak", "auth_oidc", "auth_oauth"],
     "data": [
         "security/ir.model.access.csv",

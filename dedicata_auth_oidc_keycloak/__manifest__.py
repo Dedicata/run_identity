cd2d1f54bedf3a82f74ff534aa10ebe66b6e9142
Renan Teixeira <git@renanteixeira.com.br>
2026-05-11 18:27:37 -0300

    chore(19.0): set version 19.0.1.0.0 and update website

--- dedicata_auth_oidc_keycloak/__manifest__.py
+++ dedicata_auth_oidc_keycloak/__manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Dedicata Keycloak OIDC Authentication",
     "summary": "Configure the default Keycloak OIDC provider from environment variables",
-    "version": "@@ODOO_MODULE_VERSION@@",
+    "version": "19.0.1.0.0",
     "category": "Tools",
     "license": "AGPL-3",
     "author": "Dedicata",
-    "website": "https://github.com/dedicata/odoo",
+    "website": "https://github.com/dedicata/run_identity",
     "depends": ["auth_oidc"],
     "data": [],
     "installable": True,
